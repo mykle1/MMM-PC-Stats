0d02432d40a20c27a0c94d800f69e2f1c556befb
Mykle <25211804+mykle1@users.noreply.github.com>
2018-02-03 11:05:03 -0500

    Add files via upload

--- MMM-PC-Stats.js
+++ MMM-PC-Stats.js
@@ -6,16 +6,14 @@
  */
 Module.register("MMM-PC-Stats", {
 
-    // Module config defaults.
     defaults: {
-        useHeader: false,    // true if you want a header      
-        header: "",          // Any text you want. useHeader must be true
+        useHeader: false,
+        header: "",
         maxWidth: "300px",
         animationSpeed: 0,
-        initialLoadDelay: 1250,
+        initialLoadDelay: 3250,
         retryDelay: 2500,
-        updateInterval: 60 * 1000, // Every minute
-
+        updateInterval: 60 * 1000
     },
 
     getStyles: function() {
@@ -23,19 +21,17 @@ Module.register("MMM-PC-Stats", {
     },
 
     getScripts: function() {
-		
+
         return ["moment.js"];
     },
 
-		
-	start: function() {
-        Log.info("Starting module: " + this.name);
 
-        //  Set locale.
+    start: function() {
+        Log.info("Starting module: " + this.name);
         this.Stats = {};
         this.scheduleUpdate();
     },
-	
+
 
     getDom: function() {
 
@@ -44,117 +40,60 @@ Module.register("MMM-PC-Stats", {
         wrapper.style.maxWidth = this.config.maxWidth;
 
         if (!this.loaded) {
-            wrapper.innerHTML = this.translate("Accessing CPU's & RAM . . .");
-            wrapper.classList.add("bright", "light", "small");
+            wrapper.innerHTML = this.translate("Scanning CPU and RAM . . .");
+            wrapper.classList.add("bright", "small");
             return wrapper;
         }
 
         if (this.config.useHeader != false) {
             var header = document.createElement("header");
-            header.classList.add("small", "bright", "light", "header");
+            header.classList.add("small", "bright", "header");
             header.innerHTML = this.config.header;
             wrapper.appendChild(header);
         }
 
         var Stats = this.Stats;
-		
 
-        var top = document.createElement("div");
-        top.classList.add("list-row");	
-		
-		// cpu
-        var source = document.createElement("div");
-        source.classList.add("xsmall", "bright", "cpu");
-        source.innerHTML = Stats.cpu.name;
-        wrapper.appendChild(source);
-		
-		
-		
-		// total ram
-        var source = document.createElement("div");
-        source.classList.add("xsmall", "bright", "totalRam");
-        source.innerHTML = "Total RAM = " + Stats.ram.total + Stats.ram.unit + " &nbsp &nbsp &nbsp "
-						+ " Free RAM = " + Stats.ram.free + Stats.ram.unit;
-        wrapper.appendChild(source);
-		
-		
-		
-		// cores
-        var source = document.createElement("div");
-        source.classList.add("xsmall", "bright", "cores");
-        source.innerHTML = Stats.cpu.threads[0].name + " &nbsp @ &nbsp " +  Number(Math.round(Stats.cpu.threads[0].usage+'e2')+'e-2') + "%";
-        wrapper.appendChild(source);
-		
-		
-				// cores
-        var source = document.createElement("div");
-        source.classList.add("xsmall", "bright", "cores");
-        source.innerHTML = Stats.cpu.threads[1].name + " &nbsp @ &nbsp " +  Number(Math.round(Stats.cpu.threads[1].usage+'e2')+'e-2') + "%";
-        wrapper.appendChild(source);
-		
+		// Your CPU and CPU speed
+        var yourCPU = document.createElement("div");
+        yourCPU.classList.add("small", "bright", "yourCPU");
+        yourCPU.innerHTML = Stats.cpu.name;
+        wrapper.appendChild(yourCPU);
 
-		// cores
-        var source = document.createElement("div");
-        source.classList.add("xsmall", "bright", "cores");
-        source.innerHTML = Stats.cpu.threads[2].name + " &nbsp @ &nbsp " +  Number(Math.round(Stats.cpu.threads[2].usage+'e2')+'e-2') + "%";
-        wrapper.appendChild(source);
-		
-		
-				// cores
-        var source = document.createElement("div");
-        source.classList.add("xsmall", "bright", "cores");
-        source.innerHTML = Stats.cpu.threads[3].name + " &nbsp @ &nbsp " +  Number(Math.round(Stats.cpu.threads[3].usage+'e2')+'e-2') + "%";
-        wrapper.appendChild(source);
-		
 
-		// cores
-        var source = document.createElement("div");
-        source.classList.add("xsmall", "bright", "cores");
-        source.innerHTML = Stats.cpu.threads[4].name + " &nbsp @ &nbsp " +  Number(Math.round(Stats.cpu.threads[4].usage+'e2')+'e-2') + "%";
-        wrapper.appendChild(source);
-		
-		
-				// cores
-        var source = document.createElement("div");
-        source.classList.add("xsmall", "bright", "cores");
-        source.innerHTML = Stats.cpu.threads[5].name + " &nbsp @ &nbsp " +  Number(Math.round(Stats.cpu.threads[5].usage+'e2')+'e-2') + "%";
-        wrapper.appendChild(source);
-		
+		// Your total RAM and Free RAM
+        var ram = document.createElement("div");
+        ram.classList.add("small", "bright", "ram");
+        ram.innerHTML = "Total RAM = " + Stats.ram.total + Stats.ram.unit + " &nbsp &nbsp " +
+            				" Free RAM = " + Stats.ram.free + Stats.ram.unit;
+        wrapper.appendChild(ram);
 
-		// cores
-        var source = document.createElement("div");
-        source.classList.add("xsmall", "bright", "cores");
-        source.innerHTML = Stats.cpu.threads[6].name + " &nbsp @ &nbsp " +  Number(Math.round(Stats.cpu.threads[6].usage+'e2')+'e-2') + "%";
-        wrapper.appendChild(source);
-		
-		
-				// cores
-        var source = document.createElement("div");
-        source.classList.add("xsmall", "bright", "cores");
-        source.innerHTML = Stats.cpu.threads[7].name + " &nbsp @ &nbsp " +  Number(Math.round(Stats.cpu.threads[7].usage+'e2')+'e-2') + "%";
-        wrapper.appendChild(source);
-			
+        for (var i = 0, len = Stats.cpu.threads.length; i < len; i++) {
+
+            var Element = document.createElement("div");
+            Element.classList.add("small", "bright", "cores");
+            Element.innerHTML = Stats.cpu.threads[i].name + " &nbsp  @  &nbsp " 
+								+ Number(Math.round(Stats.cpu.threads[i].usage+'e2')+'e-2') + "%";
+            wrapper.appendChild(Element);
+        }
+
         return wrapper;
-		
-    }, // closes getDom
-    
-    
-    /////  Add this function to the modules you want to control with voice //////
+
+    },
+
 
     notificationReceived: function(notification, payload) {
         if (notification === 'HIDE_STATS') {
             this.hide();
-        }  else if (notification === 'SHOW_STATS') {
+        } else if (notification === 'SHOW_STATS') {
             this.show(1000);
         }
-            
     },
-
 
     processStats: function(data) {
         this.Stats = data;
-		console.log(this.Stats);
         this.loaded = true;
+	//	console.log(this.Stats); // for checking in dev console
     },
 
     scheduleUpdate: function() {
@@ -171,7 +110,6 @@ Module.register("MMM-PC-Stats", {
     socketNotificationReceived: function(notification, payload) {
         if (notification === "STATS_RESULT") {
             this.processStats(payload);
-
             this.updateDom(this.config.animationSpeed);
         }
         this.updateDom(this.config.initialLoadDelay);
